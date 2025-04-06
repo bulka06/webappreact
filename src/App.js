3080f0428a0c5475d8c01079e87417a75164ef51
bulka06 <vbulka2021@zcollage.com.ua>
2025-04-06 17:28:08 +0300

    upd

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import React, { useEffect } from 'react';
 import './App.css';
 import useTelegram from './components/hooks/useTelegram';
 import Header from './components/Header/Header';
-
-
+import { Route, Routes } from 'react-router-dom';
+import ProductList from './components/ProductList/ProductList';
+import Form from './components/Form/Form';
 
 function App() {
   const {onToggleButton, tg} = useTelegram();
@@ -17,8 +18,11 @@ function App() {
   return (
     <div className="App">
       <Header/>
-      work
-      <button onClick={onToggleButton}> toggle </button>
+      <Routes>  
+  <Route index element={<ProductList />} />
+  <Route path="form" element={<Form />} />    
+      </Routes> 
+      
     </div>
   );
 }
---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+import './Form.css';
+import useTelegram from "../hooks/useTelegram";
+
+
+const Form = () => {
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [street, setStreet] = useState('');
+    const {tg} = useTelegram();
+
+    useEffect( () => {
+        tg.MainButton.setParams({
+            text: 'Відправити дані',
+        }); 
+    },[]);
+
+    useEffect(() => {
+        if (!name || !phone || !street) {
+            tg.MainButton.hide();
+        } else {
+            tg.MainButton.show();
+        }
+    }, [name, phone, street]);
+
+
+    const onChangeName = (e) => {
+        setName(e.target.value);
+    };
+    const onChangePhone = (e) => {
+        setPhone(e.target.value);
+    };
+    const onChangeStreet = (e) => {
+        setStreet(e.target.value);
+    };
+    
+    return (
+        <div className="form">
+            <h3>Введіть свої дані</h3>
+            <input 
+                type="text" 
+                placeholder="Ваше ім'я" 
+                className="input"
+                value={name}
+                onChange={onChangeName}
+            />
+
+            <input 
+                type="text" 
+                placeholder="Ваш номер телефону" 
+                className="input"
+                value={phone}
+                onChange={onChangePhone}
+            />
+
+            <input 
+                type="text" 
+                placeholder="Вулиця" 
+                className="input"
+                value={street}
+                onChange={onChangeStreet}
+            />
+        </div>
+    );
+};
+
+export default Form;

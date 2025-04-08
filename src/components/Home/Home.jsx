22790903d43d12c77390731e2c506c7c115ab60f
bulka06 <vbulka2021@zcollage.com.ua>
2025-04-08 14:07:31 +0300

    upd

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Home.css";
 import useTelegram from "../hooks/useTelegram";
+import homeIcon from "../assets/homeIcon.jpg"; // Змінили шлях до зображення
 
 const Home = () => {
     const { user } = useTelegram();
 
     return (
         <div className="home">
-            
+            <img src={homeIcon} alt="Головна картинка" className="home-image" />
         </div>
     );
 };

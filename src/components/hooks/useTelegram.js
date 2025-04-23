2244e92af46326895c5e1157663ee3d666361b7f
bulka06 <vbulka2021@zcollage.com.ua>
2025-04-23 20:08:28 +0300

    upd

--- src/components/hooks/useTelegram.js
+++ src/components/hooks/useTelegram.js
@@ -1,10 +1,45 @@
-export const useTelegram = () => {
-    const tg = window.Telegram.WebApp;
-  
-    return {
-      tg,
-      user: tg?.initDataUnsafe?.user,
-      queryId: tg?.initDataUnsafe?.query_id
+const tg = window.Telegram?.WebApp;
+
+const useTelegram = () => {
+    const onClose = () => {
+        if (tg) {
+            tg.close();
+        }
     };
-  };
-  
+
+    const onToggleButton = () => {
+        if (tg?.MainButton) {
+            tg.MainButton.isVisible ? tg.MainButton.hide() : tg.MainButton.show();
+        }
+    };
+
+    const sendData = (data) => {
+        if (tg) {
+            tg.sendData(JSON.stringify(data));
+        }
+    };
+
+    const onMainButtonClick = (callback) => {
+        if (tg) {
+            tg.onEvent('mainButtonClicked', callback);
+        }
+    };
+
+    const offMainButtonClick = (callback) => {
+        if (tg) {
+            tg.offEvent('mainButtonClicked', callback);
+        }
+    };
+
+    return {
+        tg,
+        user: tg?.initDataUnsafe?.user,
+        onClose,
+        onToggleButton,
+        sendData,
+        onMainButtonClick,
+        offMainButtonClick,
+    };
+};
+
+export default useTelegram;

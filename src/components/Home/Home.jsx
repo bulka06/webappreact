3efa16a7444ed3f917e6ecf8dae93f63fdeaebd6
bulka06 <vbulka2021@zcollage.com.ua>
2025-04-28 10:40:09 +0300

    upd

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,8 +3,7 @@ import { useLocation } from "react-router-dom";
 
 import "./Home.css";
 import ZakladList from "./ZakladList/ZakladList";
-import HomeButtons from "./HomeButtons/HomeButtons";
-import CategoryList from "./CategoryList/CategoryList";
+
 import DeliveryInfo from "./DeliveryInfo/DeliveryInfo"; 
 import AboutUs from "./AboutUs/AboutUs";
 
@@ -30,17 +29,10 @@ const Home = () => {
                     <h1>Доставка вашої улюбленої їжі</h1>
                 </div>
             </div>
-            <HomeButtons
-                onScrollToZaklads={() => zakladRef.current?.scrollIntoView({ behavior: "smooth" })}
-                onScrollToCategories={() => categoryRef.current?.scrollIntoView({ behavior: "smooth" })}
-            />
-
+           
             <div className="home-list-wrapper">
             <div ref={zakladRef} className="scroll-target">
              <ZakladList />
-            </div>
-            <div ref={categoryRef} className="scroll-target">
-             <CategoryList />
             </div>
             
                 <div ref={deliveryRef} id="delivery-info">

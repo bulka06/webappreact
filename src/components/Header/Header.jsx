91c2b848b21bb8b63d4eda94f59a3428502e1c29
bulka06 <vbulka2021@zcollage.com.ua>
2025-04-28 11:11:23 +0300

    upd

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 import "./Header.css";
 import HeaderBar from "../HeaderBar/HeaderBar";
+import { FiShoppingCart } from 'react-icons/fi';
+import { useBask } from '../Basket/BaskContext/BaskContext';
+
+
+import { useNavigate } from "react-router-dom";
+
 
 const Header = ({ onOpenModal }) => {
     const [isHeaderBarOpen, setHeaderBarOpen] = useState(false);
+    const { baskItems } = useBask();
+    const navigate = useNavigate();
 
     const toggleHeaderBar = () => {
         setHeaderBarOpen(!isHeaderBarOpen);
     };
+
+    const handleCartClick = () => {
+        navigate('/bask');
+    };
+
+    const totalPrice = baskItems?.reduce(
+        (total, item) => total + item.price * item.quantity,
+        0
+    ) || 0;
 
     return (
         <>
@@ -25,13 +42,14 @@ const Header = ({ onOpenModal }) => {
                         <strong>FastGo</strong>
                     </div>
                 </div>
-                <div className="right">
-                    <span className="icon"></span>
-                    <span className="cart-count"></span>
+                <div className="right" onClick={handleCartClick}>
+                    <FiShoppingCart className="cart-icon" />
+                    {totalPrice > 0 && (
+                        <span className="cart-price">{totalPrice} ₴</span>
+                    )}
                 </div>
             </header>
 
-            {/* Передаємо onOpenModal у HeaderBar */}
             <HeaderBar 
                 isOpen={isHeaderBarOpen} 
                 onClose={toggleHeaderBar}
